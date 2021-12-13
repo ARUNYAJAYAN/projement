fa71cd41fcb610c37014cd23017263eb45e613a8
CYCLOIDES\CY0166 <arunya.j@cycloides.co.in>
2021-12-13 19:22:33 +0530

    APIs for projement
    Signed-off-by: CYCLOIDES\CY0166 <arunya.j@cycloides.co.in>

---
+++ company/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from django.utils.decorators import method_decorator
+from utils.decorators import allowed_users
+from company.models import Company
+from company.serializers import CompanySerializer
+from rest_framework import authentication, permissions
+from utils.decorators import allowed_users
+
+
+class CompanyList(generics.ListCreateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    allowed_users(allowed_roles=['admin'])
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({"status": status.HTTP_400_BAD_REQUEST, "errors": serializer.errors, "message": "Something went wrong"})
+        else:
+            serializer.save()
+            return Response({"status": status.HTTP_201_CREATED, "data": serializer.data, "message": "Your details saved successfully"})
+
+    allowed_users(allowed_roles=['admin', 'Developer'])
+    def list(self, request):
+        serializer = self.get_serializer(self.get_queryset(), many=True)
+
+        return Response({"status": status.HTTP_200_OK, "data": serializer.data, "message": "Companies list"})
+
+
+class CompanyDetail(generics.RetrieveAPIView):
+    queryset = Company
+    serializer_class = CompanySerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    allowed_users(allowed_roles=['admin', 'Developer'])
+    def retrieve(self, request, *args, **kwargs):
+        serializer = self.get_serializer(self.get_object(), many=False)
+        return Response({"status": status.HTTP_200_OK, "data": serializer.data, "message": "Company's Detail"})
